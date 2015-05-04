e086ef2f7f9aa146936b3a6889bbcfb1032742ca
HongyiZhu <hongyi.zhu.21@gmail.com>
2015-05-04 16:20:11 -0700

    modified a printing backend script

--- weather/backend.py
+++ weather/backend.py
@@ -2,6 +2,7 @@ __author__ = 'Hongyi'
 
 import weather_realtime
 import pollution_realtime
+import sys
 
 
 def get_api_return(lat, lon, date, hour):
@@ -16,6 +17,8 @@ def get_api_return(lat, lon, date, hour):
         string += ", "
     string += "?"
 
-    return string
+    print(string)
 
 # print(get_api_return(32.2217, -110.9264, "2015-5-3", 16))
+if __name__ == "__main__":
+   get_api_return(float(sys.argv[1]), float(sys.argv[2]), sys.argv[3], int(sys.argv[4]))
