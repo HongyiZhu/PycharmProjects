9240abda734af2745cbc17f474f64674f428ca58
HongyiZhu <hongyi.zhu.21@gmail.com>
2015-08-18 13:58:03 -0700

    build master structure

--- General_Modules/multithreading/src/master.py
+++ General_Modules/multithreading/src/master.py
@@ -1,1 +1,24 @@
 __author__ = 'zhuhy'
+
+import subprocess
+import time
+
+# Global Configuration
+python_path = "C:/Python34/python.exe"
+slave_path = "C:/Users/zhuhy/Documents/GitHub/PycharmProjects/General_Modules/multithreading/src/slave.py"
+
+# Modify iterange according to the need
+overall_scale = 10000
+slice_size = 100
+iterange = 0
+
+for iter in range(iterange):
+    # Append parameters into the args list
+    args = [python_path, slave_path]
+    for i in range(1,4):
+        args.append('%s' % str(i))
+
+    # Subprocess the slaves.
+    proc = subprocess.Popen(args)
+    while proc.poll() == None:
+        time.sleep(1)
