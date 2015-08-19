f4a600e01bf02bb9a9d2755d30420c0c6e5d755c
HongyiZhu <hongyi.zhu.21@gmail.com>
2015-08-19 03:13:24 -0700

    try again

--- nano/USPTO.py
+++ nano/USPTO.py
@@ -1,1 +1,74 @@
 __author__ = 'Hongyi'
+
+import USPTO_tool
+
+# Country List
+country_f = open('country_uspto.txt', 'r')
+country_list = []
+for line in country_f:
+    country_list.append(line.strip())
+country_f.close()
+
+# Two sets of queries
+new_f = open('new_uspto_query.txt', "r")
+old_f = open('old_uspto_query.txt', 'r')
+new_query = []
+for line in new_f:
+    new_query.append(line.strip().replace(" ", "+") + "%0D%0A")
+old_query = []
+for line in old_f:
+    old_query.append(line.strip().replace(" ", "+") + "%0D%0A")
+new_f.close()
+old_f.close()
+
+# Year range
+starting = 2000
+ending = 2014
+
+# URL base
+base = "http://patft.uspto.gov/netacgi/nph-Parser?Sect1=PTO2&Sect2=HITOFF" \
+       "&u=%2Fnetahtml%2FPTO%2Fsearch-adv.htm&r=0&p=1&f=S&l=50&Query="
+# iterate on countries
+for country in country_list:
+    # iterate on year
+    for year in range(starting, ending + 1):
+        pat_list = []   # patent list for each year
+        if year > 2010:
+            # Query with new keywords
+            for key in new_query:
+                # generate query link
+                query = "isd/%s+AND+acn/%s+AND+(ttl/%s+OR+abst/%s+OR+aclm/%s)" % (str(year), country, key, key, key)
+                query = query.replace("/", "%2F").replace("(", "%28").replace(")", "%29")\
+                    .replace(" ", "+").replace("\"", "%22").replace("$", "%24")
+                url = base + query + "&d=PTXT"
+
+                # parse the query result
+                key_result = USPTO_tool.analyze_url(url)
+
+                # remove duplicate entry
+                for result in key_result:
+                    if result in pat_list:
+                        pass
+                    else:
+                        pat_list.append(result)
+        else:
+            # Query with old keywords
+            for key in old_query:
+                # generate query link
+                query = "isd/%s+AND+acn/%s+AND+(ttl/%s+OR+abst/%s+OR+aclm/%s)" % (str(year), country, key, key, key)
+                query = query.replace("/", "%2F").replace("(", "%28").replace(")", "%29")\
+                    .replace(" ", "+").replace("\"", "%22").replace("$", "%24")
+                url = base + query + "&d=PTXT"
+
+                # Parse query result
+                key_result = USPTO_tool.analyze_url(url)
+
+                # remove duplicate entry
+                for result in key_result:
+                    if result in pat_list:
+                        pass
+                    else:
+                        pat_list.append(result)
+
+        # patent number for each year
+        pat_num = len(pat_list)
--- nano/USPTO_tool.py
+++ nano/USPTO_tool.py
@@ -1,1 +1,81 @@
 __author__ = 'Hongyi'
+
+import re
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+def next_page(soup):
+    """
+    Generate link to the next page
+    :param soup: The HTML page
+    :return: The URL to next page
+    """
+    query_base = "http://patft.uspto.gov/netacgi/nph-Parser?"
+
+    # Retrieve the right form, break if found
+    for f in soup.find_all('form'):
+        if "Next" in f.find_all('input')[-1]['name']:
+            form = f
+            break
+
+    # find all the parameters and generate the url
+    input_list = form.find_all('input')
+    str_list = [(x['name'] + "=" + x['value']).replace("/", "%2F").replace("(", "%28").replace(")", "%29")\
+                       .replace(" ", "+").replace("\"", "%22").replace("$", "%24") for x in input_list]
+
+    query_str = "&".join(str_list)
+    return query_base + query_str
+
+
+def analyze(soup, patent_list):
+    """
+    Analyze the patent page and extract the patent number
+    :param soup: HTML page
+    :param patent_list: patent list of the keyword
+    :return:
+    """
+    tr_list = soup.find_all('table')[1].find_all('tr')[1:]
+    for tr in tr_list:
+        patent_list.append(tr.find_all('td')[1].text)
+
+
+def analyze_url(url):
+    """
+    Analyze the query page and gather patent information
+    :param url: the query link
+    :return: pat_list: a list of patents containing keyword
+    """
+    page = urlopen(url)
+    soup = BeautifulSoup(page)
+    totalnumber = 0
+    patent_list = []
+
+    # get general number of patents
+    m = re.search(": (\\d+) patents", soup.text)
+    if m is not None:
+        totalnumber = int(m.group(1))
+
+    # no result => return a null list
+    if totalnumber == 0:
+        return patent_list
+    # else parse through the webpage
+    else:
+        pagenum = 1
+
+        # analyze the table elements
+        analyze(soup, patent_list)
+
+        # flip to new pages if needed
+        while pagenum * 50 < totalnumber:
+            pagenum += 1
+            # get url to nex page
+            newurl = next_page(soup)
+
+            # analyze pages one by one
+            page = urlopen(newurl)
+            soup = BeautifulSoup(page)
+            analyze(soup, patent_list)
+
+    # return after traverse
+    return patent_list
