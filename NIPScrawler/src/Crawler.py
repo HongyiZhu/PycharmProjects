43e2774c7f7f1083426cb48bb2104a0871ee2c4c
HongyiZhu <hongyi.zhu.21@gmail.com>
2016-04-01 11:07:07 -0700

    modulate ViewPage.py

--- NIPScrawler/src/Crawler.py
+++ NIPScrawler/src/Crawler.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from NIPScrawler.src.ViewPage import ViewPage
 
 browser = webdriver.PhantomJS()
 
-browser.get('http://www.google.com')
+root = 'http://papers.nips.cc'
+
+browser.get('http://papers.nips.cc/book/advances-in-neural-information-processing-systems-27-2014')
+soup = BeautifulSoup(browser.page_source, 'lxml')
+anchor_list = soup.find_all('a')
+paper_link = [x.get('href') for x in anchor_list if "paper" in x.get('href')]
+
+test = ViewPage(urljoin(root, paper_link[0]), r'D:/NIPS/paper')
+test.parse_page()
+
 browser.quit()
---
+++ NIPScrawler/src/ViewPage.py
@@ -0,0 +1,66 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+from urllib.request import urlretrieve
+import re
+import time
+import random
+import os
+
+root = 'http://papers.nips.cc'
+
+
+def get_title(page):
+    subtitle = page.find('h2', {'class': 'subtitle'}).text
+    return subtitle
+
+
+def get_abstract(page):
+    abstract = page.find('p', {'class': 'abstract'}).text
+    return abstract
+
+
+def get_author(page):
+    ul = page.find('ul', {'class': 'authors'})
+    author_dict = {}
+    author_list = ul.findAll('a')
+    for anchor in author_list:
+        author_dict[anchor.text] = urljoin(root, anchor.get('href'))
+    return author_dict
+
+
+def get_id(url):
+    reg = re.compile(r'paper/(\d+)-\w')
+    paper_id = reg.findall(url)
+    return paper_id
+
+
+class ViewPage:
+    def __init__(self, url, folder):
+        self.url = url
+        self.folder = folder
+        self.browser = webdriver.PhantomJS()
+        self.browser.get(self.url)
+        self.source = self.browser.page_source
+        self.id = get_id(url)
+
+    def parse_page(self):
+        page = BeautifulSoup(self.source, 'lxml')
+        title = get_title(page)
+        authors = get_author(page)
+        abstract = get_abstract(page)
+        event_type = self.get_event_type()
+        local_filepath = self.download_file(page)
+
+    def get_event_type(self):
+        reg = re.compile(r'Conference Event Type: (.*)</h3>')
+        conference_event_type = reg.findall(self.source)[0]
+        return conference_event_type
+
+    def download_file(self, page):
+        pdf_href = page.find('a', href=True, text='[PDF]')['href']
+        filename = os.path.join(self.folder, pdf_href.split('/')[2])
+        time.sleep(random.randrange(10))
+        urlretrieve(urljoin(root, pdf_href), filename)
+        self.browser.close()
+        return filename
