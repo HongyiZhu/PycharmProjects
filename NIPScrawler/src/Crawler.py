878731259a05034add8392dbd13283a0bdb7cb4a
HongyiZhu <hongyi.zhu.21@gmail.com>
2016-04-01 18:58:36 -0700

    add year into the crawler

--- NIPScrawler/src/Crawler.py
+++ NIPScrawler/src/Crawler.py
@@ -7,12 +7,16 @@ browser = webdriver.PhantomJS()
 
 root = 'http://papers.nips.cc'
 
-browser.get('http://papers.nips.cc/book/advances-in-neural-information-processing-systems-27-2014')
+number = 27
+year = 2014
+
+browser.get('http://papers.nips.cc/book/advances-in-neural-information-processing-systems-'
+            + str(number) + '-' + str(year))
 soup = BeautifulSoup(browser.page_source, 'lxml')
 anchor_list = soup.find_all('a')
 paper_link = [x.get('href') for x in anchor_list if "paper" in x.get('href')]
 
-test = ParsePage(urljoin(root, paper_link[0]), r'D:/NIPS/paper')
+test = ParsePage(urljoin(root, paper_link[0]), r'C:/Users/zhuhy/Documents/NIPS/paper', year)
 test.parse_page()
 
 browser.quit()
--- NIPScrawler/src/Database_IO.py
+++ NIPScrawler/src/Database_IO.py
@@ -10,15 +10,19 @@ def db_connect_new():
                            charset='utf8',
                            autocommit=True).cursor()
 
-
 class DatabaseConnection:
 
     def __init__(self):
         self.cursor = db_connect_new()
 
-    def insert(self, title, authors, abstract, event_type, local_filepath):
-        query = ""
-        self.cursor.execute(query)
+    def insert(self, title, authors, abstract, event_type, year, local_filepath):
+        print(title)
+        print(abstract)
+        print(event_type)
+        print(local_filepath)
+        for author in authors:
+            print(author + " => " + authors[author])
+        # self.cursor.execute(query)
 
     def close(self):
         self.cursor.close()
